6c548df24df45ff1928b4564655e118f9b13df3c
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-30 11:46:06 +0100

    Icons

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -42,7 +43,7 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows="3"
         />
-        <button onClick={submitNote}>Add</button>
+        <button onClick={submitNote}><AddIcon /></button>
       </form>
     </div>
   );
