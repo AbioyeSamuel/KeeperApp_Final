3fd938136fe24460a316848ca40148b18861e88b
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-07-01 20:34:35 +0100

    redo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,8 @@ import CreateArea from "./CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
-  }
-
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
